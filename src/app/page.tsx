f68b0f433cdf0604e4bbbda9f5d638c710c8cd9c
Cyro <85706992+cyrqrz@users.noreply.github.com>
2025-02-03 20:40:25 -0300

    feat: improves metadata and SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,38 +13,63 @@ import About from "../../components/About/About";
 import Contact from "../../components/Contact/Contact";
 import Faces from "../../components/Faces/Faces";
 
+import Head from "next/head";
+
 const HomePage = () => {
   return (
-    <div className="min-h-screen !scroll-smooth">
-      {/* Hero Section */}
-      <Hero></Hero>x1    {/* Features Section */}
-      <PitchSection></PitchSection>
-      {/* About the House Section */}
-      <section id="sobre-casa" className="bg-white py-20">
-        <div className="Flex-auto">
-          <h2 className="text-3xl font-bold text-center mb-2">Sobre a Casa</h2>
-          <p className="text-gray-600 max-w-2xl mx-auto mb-10 text-center">
-            Esse aqui será o lugar em que você passará os próximos (melhores)
-            dias da sua vida.
-          </p>
-          <div className="grid md:grid-cols-2 gap-8 max-w-6xl mx-auto px-4 bg-white border border-gray-300 rounded-xl p-6 shadow-md transition duration-300">
-            <Map>{/*Map component*/}</Map>
-            {/* Carousel */}
-            <Carousel></Carousel>
+    <>
+      <Head>
+        <meta
+          property="og:title"
+          content="República Dauhma - Estudantes Universitários em São Carlos"
+        />
+        <meta
+          property="og:description"
+          content="A República Dauhma é uma comunidade de estudantes universitários em São Carlos, oferecendo um ambiente de moradia e convivência para jovens universitários."
+        />
+        <meta property="og:image" content="https://i.ibb.co/1fFyhrXs/image.png" />
+        
+        <title>
+          República Dauhma - Estudantes Universitários em São Carlos
+        </title>
+        <meta
+          name="description"
+          content="A República Dauhma é uma comunidade de estudantes universitários em São Carlos, oferecendo um ambiente de moradia e convivência para jovens universitários."
+        />
+      </Head>
+      <div className="min-h-screen !scroll-smooth">
+        {/* Hero Section */}
+        <Hero></Hero>
+        {/* Features Section */}
+        <PitchSection></PitchSection>
+        {/* About the House Section */}
+        <section id="sobre-casa" className="bg-white py-20">
+          <div className="Flex-auto">
+            <h2 className="text-3xl font-bold text-center mb-2">
+              Sobre a Casa
+            </h2>
+            <p className="text-gray-600 max-w-2xl mx-auto mb-10 text-center">
+              Esse aqui será o lugar em que você passará os próximos (melhores)
+              dias da sua vida.
+            </p>
+            <div className="grid md:grid-cols-2 gap-8 max-w-6xl mx-auto px-4 bg-white border border-gray-300 rounded-xl p-6 shadow-md transition duration-300">
+              <Map>{/*Map component*/}</Map>
+              {/* Carousel */}
+              <Carousel></Carousel>
+            </div>
+            <div>
+              <Pins></Pins>
+            </div>
           </div>
-          <div>
-            <Pins></Pins>
-          </div>
-        </div>1
-        <DownButton></DownButton>
-      </section>
-      {/* About Us Section */}
- 
-      <About></About>
-      <Faces></Faces>
-      {/* Contact Section */}
-      <Contact></Contact>
-    </div>
+          <DownButton></DownButton>
+        </section>
+        {/* About Us Section */}
+        <About></About>
+        <Faces></Faces>
+        {/* Contact Section */}
+        <Contact></Contact>
+      </div>
+    </>
   );
 };
 export default HomePage;
