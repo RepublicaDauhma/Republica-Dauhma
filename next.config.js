1d6f619f140c8c566cc71923266a78094fa3c692
Cyro <85706992+cyrqrz@users.noreply.github.com>
2025-02-04 13:04:04 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /**
  * @type {import('next').NextConfig}
  */
-
-const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-  basePath: isProd ? '/republica-dauhma' : '',
+  basePath: '/republica-dauhma',
   output: 'export',
   distDir: 'dist',
   images: {
